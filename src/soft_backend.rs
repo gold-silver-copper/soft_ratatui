ced2aca90d60c0eec2333319b5d471002c5400e4
zombkit <stephen.korzen@gmail.com>
2025-04-27 00:30:19 -0700

    try some stuff

--- src/soft_backend.rs
+++ src/soft_backend.rs
@@ -3,16 +3,16 @@
 
 use std::io;
 
+use crate::colors::*;
 use cosmic_text::Style;
-use ratatui::backend::{Backend, ClearType, WindowSize};
-use ratatui::buffer::{Buffer, Cell};
-use ratatui::layout::{Position, Rect, Size};
-use ratatui::style::{Color as RatColor, Modifier};
-
 use cosmic_text::{
     Attrs, Buffer as CosmicBuffer, Color as CosmicColor, Family, FontSystem, Metrics, Shaping,
     SwashCache, Weight,
 };
+use ratatui::backend::{Backend, ClearType, WindowSize};
+use ratatui::buffer::{Buffer, Cell};
+use ratatui::layout::{Position, Rect, Size};
+use ratatui::style::{Color as RatColor, Modifier};
 use tiny_skia::{
     BlendMode, Color as SkiaColor, ColorU8 as SkiaColorU8, FilterQuality, IntSize, Paint, Pixmap,
     PixmapMut, PixmapPaint, PremultipliedColorU8, Rect as SkiaRect, Transform,
@@ -224,6 +224,7 @@ impl Backend for SoftBackend {
             self.draw_cell(&c, x, y);
             //   println!("{c:#?}");
         }
+        //  self.draw_buffer();
 
         // Save to PNG file
         self.screen_pixmap.save_png("output_tiny_skia.png").unwrap();
@@ -288,6 +289,11 @@ impl SoftBackend {
         let buffer_width = self.buffer.area.width;
         let buffer_height = self.buffer.area.height;
         let attrs = Attrs::new().family(Family::Monospace);
+        self.character_buffer.set_size(
+            &mut self.font_system,
+            Some((self.char_width * buffer_width as u32) as f32),
+            Some((self.char_height * buffer_height as u32) as f32),
+        );
 
         let mut text_vec = Vec::new();
 
@@ -352,107 +358,5 @@ impl SoftBackend {
                 }
             },
         );
-    }
-}
-
-/// Convert a ratatui color to a tiny-skia SkiaColor
-pub fn rat_to_skia_color(rat_col: &RatColor, is_a_fg: bool) -> SkiaColor {
-    match rat_col {
-        RatColor::Reset => {
-            if is_a_fg {
-                SkiaColor::from_rgba8(204, 204, 255, 255)
-            } else {
-                SkiaColor::from_rgba8(15, 15, 112, 255)
-            }
-        }
-        RatColor::Black => SkiaColor::from_rgba8(0, 0, 0, 255),
-        RatColor::Red => SkiaColor::from_rgba8(139, 0, 0, 255),
-        RatColor::Green => SkiaColor::from_rgba8(0, 100, 0, 255),
-        RatColor::Yellow => SkiaColor::from_rgba8(255, 215, 0, 255),
-        RatColor::Blue => SkiaColor::from_rgba8(0, 0, 139, 255),
-        RatColor::Magenta => SkiaColor::from_rgba8(99, 9, 99, 255),
-        RatColor::Cyan => SkiaColor::from_rgba8(0, 0, 255, 255),
-        RatColor::Gray => SkiaColor::from_rgba8(128, 128, 128, 255),
-        RatColor::DarkGray => SkiaColor::from_rgba8(64, 64, 64, 255),
-        RatColor::LightRed => SkiaColor::from_rgba8(255, 0, 0, 255),
-        RatColor::LightGreen => SkiaColor::from_rgba8(0, 255, 0, 255),
-        RatColor::LightBlue => SkiaColor::from_rgba8(173, 216, 230, 255),
-        RatColor::LightYellow => SkiaColor::from_rgba8(255, 255, 224, 255),
-        RatColor::LightMagenta => SkiaColor::from_rgba8(139, 0, 139, 255),
-        RatColor::LightCyan => SkiaColor::from_rgba8(224, 255, 255, 255),
-        RatColor::White => SkiaColor::from_rgba8(255, 255, 255, 255),
-        RatColor::Indexed(i) => {
-            // You can customize this mapping, or use a fixed 256-color palette lookup
-            let i = *i as u8;
-            SkiaColor::from_rgba8(i.wrapping_mul(i), i.wrapping_add(i), i, 255)
-        }
-        RatColor::Rgb(r, g, b) => SkiaColor::from_rgba8(*r, *g, *b, 255),
-    }
-}
-
-pub fn rat_to_skia_coloru8(rat_col: &RatColor, is_a_fg: bool) -> SkiaColorU8 {
-    match rat_col {
-        RatColor::Reset => {
-            if is_a_fg {
-                SkiaColorU8::from_rgba(204, 204, 255, 255)
-            } else {
-                SkiaColorU8::from_rgba(15, 15, 112, 255)
-            }
-        }
-        RatColor::Black => SkiaColorU8::from_rgba(0, 0, 0, 255),
-        RatColor::Red => SkiaColorU8::from_rgba(139, 0, 0, 255),
-        RatColor::Green => SkiaColorU8::from_rgba(0, 100, 0, 255),
-        RatColor::Yellow => SkiaColorU8::from_rgba(255, 215, 0, 255),
-        RatColor::Blue => SkiaColorU8::from_rgba(0, 0, 139, 255),
-        RatColor::Magenta => SkiaColorU8::from_rgba(99, 9, 99, 255),
-        RatColor::Cyan => SkiaColorU8::from_rgba(0, 0, 255, 255),
-        RatColor::Gray => SkiaColorU8::from_rgba(128, 128, 128, 255),
-        RatColor::DarkGray => SkiaColorU8::from_rgba(64, 64, 64, 255),
-        RatColor::LightRed => SkiaColorU8::from_rgba(255, 0, 0, 255),
-        RatColor::LightGreen => SkiaColorU8::from_rgba(0, 255, 0, 255),
-        RatColor::LightBlue => SkiaColorU8::from_rgba(173, 216, 230, 255),
-        RatColor::LightYellow => SkiaColorU8::from_rgba(255, 255, 224, 255),
-        RatColor::LightMagenta => SkiaColorU8::from_rgba(139, 0, 139, 255),
-        RatColor::LightCyan => SkiaColorU8::from_rgba(224, 255, 255, 255),
-        RatColor::White => SkiaColorU8::from_rgba(255, 255, 255, 255),
-        RatColor::Indexed(i) => {
-            // You can customize this mapping, or use a fixed 256-color palette lookup
-            let i = *i as u8;
-            SkiaColorU8::from_rgba(i.wrapping_mul(i), i.wrapping_add(i), i, 255)
-        }
-        RatColor::Rgb(r, g, b) => SkiaColorU8::from_rgba(*r, *g, *b, 255),
-    }
-}
-
-pub fn rat_to_cosmic_color(rat_col: &RatColor, is_a_fg: bool) -> CosmicColor {
-    match rat_col {
-        RatColor::Reset => {
-            if is_a_fg {
-                CosmicColor::rgba(204, 204, 255, 255)
-            } else {
-                CosmicColor::rgba(15, 15, 112, 255)
-            }
-        }
-        RatColor::Black => CosmicColor::rgba(0, 0, 0, 255),
-        RatColor::Red => CosmicColor::rgba(139, 0, 0, 255),
-        RatColor::Green => CosmicColor::rgba(0, 100, 0, 255),
-        RatColor::Yellow => CosmicColor::rgba(255, 215, 0, 255),
-        RatColor::Blue => CosmicColor::rgba(0, 0, 139, 255),
-        RatColor::Magenta => CosmicColor::rgba(99, 9, 99, 255),
-        RatColor::Cyan => CosmicColor::rgba(0, 0, 255, 255),
-        RatColor::Gray => CosmicColor::rgba(128, 128, 128, 255),
-        RatColor::DarkGray => CosmicColor::rgba(64, 64, 64, 255),
-        RatColor::LightRed => CosmicColor::rgba(255, 0, 0, 255),
-        RatColor::LightGreen => CosmicColor::rgba(0, 255, 0, 255),
-        RatColor::LightBlue => CosmicColor::rgba(173, 216, 230, 255),
-        RatColor::LightYellow => CosmicColor::rgba(255, 255, 224, 255),
-        RatColor::LightMagenta => CosmicColor::rgba(139, 0, 139, 255),
-        RatColor::LightCyan => CosmicColor::rgba(224, 255, 255, 255),
-        RatColor::White => CosmicColor::rgba(255, 255, 255, 255),
-        RatColor::Indexed(i) => {
-            let i = *i as u8;
-            CosmicColor::rgba(i.wrapping_mul(i), i.wrapping_add(i), i, 255)
-        }
-        RatColor::Rgb(r, g, b) => CosmicColor::rgba(*r, *g, *b, 255),
     }
 }
