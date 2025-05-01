a70e480cc54aba283835b2df57bd4035100faca9
zombkit <stephen.korzen@gmail.com>
2025-05-01 12:07:19 -0700

    rgb

--- src/pixmap.rs
+++ src/pixmap.rs
@@ -1,19 +1,19 @@
 /// A pixmap with RGBA pixels stored in a flat vector.
 #[derive(Debug, Clone)]
-pub struct RgbaPixmap {
+pub struct RgbPixmap {
     width: usize,
     height: usize,
     data: Vec<u8>,
 }
 
-impl RgbaPixmap {
+impl RgbPixmap {
     /// Creates a new pixmap filled with the specified RGBA color.
     ///
     /// (new width height color) -> RgbaPixmap
     ///
     /// * color : [u8; 4] - RGBA pixel
     pub fn new(width: usize, height: usize) -> Self {
-        let data = [0, 0, 0, 255].repeat(width * height);
+        let data = [0, 0, 0].repeat(width * height);
         Self {
             width,
             height,
@@ -24,32 +24,32 @@ impl RgbaPixmap {
     /// Sets the RGBA value of a pixel at (x, y).
     ///
     /// * color : [u8; 4] - RGBA pixel
-    pub fn put_pixel(&mut self, x: usize, y: usize, color: [u8; 4]) {
+    pub fn put_pixel(&mut self, x: usize, y: usize, color: [u8; 3]) {
         assert!(
             x < self.width && y < self.height,
             "Pixel coordinates out of bounds"
         );
-        let index = 4 * (y * self.width + x);
-        self.data[index..index + 4].copy_from_slice(&color);
+        let index = 3 * (y * self.width + x);
+        self.data[index..index + 3].copy_from_slice(&color);
     }
 
     /// Returns the RGBA value of a pixel at (x, y).
     ///
     /// -> [u8; 4] - RGBA pixel
-    pub fn get_pixel(&self, x: usize, y: usize) -> [u8; 4] {
+    pub fn get_pixel(&self, x: usize, y: usize) -> [u8; 3] {
         assert!(
             x < self.width && y < self.height,
             "Pixel coordinates out of bounds"
         );
-        let index = 4 * (y * self.width + x);
-        self.data[index..index + 4].try_into().unwrap()
+        let index = 3 * (y * self.width + x);
+        self.data[index..index + 3].try_into().unwrap()
     }
 
     /// Fills the entire pixmap with the specified RGBA color.
     ///
     /// * color : [u8; 4] - RGBA pixel
-    pub fn fill(&mut self, color: [u8; 4]) {
-        for chunk in self.data.chunks_mut(4) {
+    pub fn fill(&mut self, color: [u8; 3]) {
+        for chunk in self.data.chunks_mut(3) {
             chunk.copy_from_slice(&color);
         }
     }
