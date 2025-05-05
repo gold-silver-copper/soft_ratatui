292e7a9b93785cca0f8720025c7678b36641bfea
zombkit <stephen.korzen@gmail.com>
2025-05-05 16:30:40 -0700

    ssss

--- src/colors.rs
+++ src/colors.rs
@@ -1,36 +1,36 @@
 use cosmic_text::{Attrs, Color as CosmicColor};
 use ratatui::style::Color as RatColor;
 
-pub fn rat_to_rgba(rat_col: &RatColor, is_a_fg: bool) -> [u8; 4] {
+pub fn rat_to_rgb(rat_col: &RatColor, is_a_fg: bool) -> [u8; 3] {
     match rat_col {
         RatColor::Reset => {
             if is_a_fg {
-                [204, 204, 255, 255] // foreground reset color
+                [204, 204, 255] // foreground reset color
             } else {
-                [15, 15, 112, 255] // background reset color
+                [15, 15, 112] // background reset color
             }
         }
-        RatColor::Black => [0, 0, 0, 255],
-        RatColor::Red => [139, 0, 0, 255],
-        RatColor::Green => [0, 100, 0, 255],
-        RatColor::Yellow => [255, 215, 0, 255],
-        RatColor::Blue => [0, 0, 139, 255],
-        RatColor::Magenta => [99, 9, 99, 255],
-        RatColor::Cyan => [0, 0, 255, 255],
-        RatColor::Gray => [128, 128, 128, 255],
-        RatColor::DarkGray => [64, 64, 64, 255],
-        RatColor::LightRed => [255, 0, 0, 255],
-        RatColor::LightGreen => [0, 255, 0, 255],
-        RatColor::LightBlue => [173, 216, 230, 255],
-        RatColor::LightYellow => [255, 255, 224, 255],
-        RatColor::LightMagenta => [139, 0, 139, 255],
-        RatColor::LightCyan => [224, 255, 255, 255],
-        RatColor::White => [255, 255, 255, 255],
+        RatColor::Black => [0, 0, 0],
+        RatColor::Red => [139, 0, 0],
+        RatColor::Green => [0, 100, 0],
+        RatColor::Yellow => [255, 215, 0],
+        RatColor::Blue => [0, 0, 139],
+        RatColor::Magenta => [99, 9, 99],
+        RatColor::Cyan => [0, 0, 255],
+        RatColor::Gray => [128, 128, 128],
+        RatColor::DarkGray => [64, 64, 64],
+        RatColor::LightRed => [255, 0, 0],
+        RatColor::LightGreen => [0, 255, 0],
+        RatColor::LightBlue => [173, 216, 230],
+        RatColor::LightYellow => [255, 255, 224],
+        RatColor::LightMagenta => [139, 0, 139],
+        RatColor::LightCyan => [224, 255, 255],
+        RatColor::White => [255, 255, 255],
         RatColor::Indexed(i) => {
             let i = *i as u8;
-            [i.wrapping_mul(i), i.wrapping_add(i), i, 255]
+            [i.wrapping_mul(i), i.wrapping_add(i), i]
         }
-        RatColor::Rgb(r, g, b) => [*r, *g, *b, 255],
+        RatColor::Rgb(r, g, b) => [*r, *g, *b],
     }
 }
 
