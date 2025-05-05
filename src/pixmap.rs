23b25f22d944a34f345ef89130a3b3a63e8ed07d
zombkit <stephen.korzen@gmail.com>
2025-05-05 16:45:41 -0700

    s

--- src/pixmap.rs
+++ src/pixmap.rs
@@ -7,11 +7,11 @@ pub struct RgbPixmap {
 }
 
 impl RgbPixmap {
-    /// Creates a new pixmap filled with the specified RGBA color.
+    /// Creates a new pixmap.
     ///
-    /// (new width height color) -> RgbaPixmap
+    /// (new width height color) -> RgbPixmap
     ///
-    /// * color : [u8; 4] - RGBA pixel
+    /// * color : [u8; 3] - RGB pixel
     pub fn new(width: usize, height: usize) -> Self {
         let data = [0, 0, 0].repeat(width * height);
         Self {
@@ -21,9 +21,9 @@ impl RgbPixmap {
         }
     }
 
-    /// Sets the RGBA value of a pixel at (x, y).
+    /// Sets the RGB value of a pixel at (x, y).
     ///
-    /// * color : [u8; 4] - RGBA pixel
+    /// * color : [u8; 3] - RGB pixel
     pub fn put_pixel(&mut self, x: usize, y: usize, color: [u8; 3]) {
         /*  println!(
             "tried to put pixel at {},{} on screen size of {},{}",
@@ -37,9 +37,9 @@ impl RgbPixmap {
         self.data[index..index + 3].copy_from_slice(&color);
     }
 
-    /// Returns the RGBA value of a pixel at (x, y).
+    /// Returns the RGB value of a pixel at (x, y).
     ///
-    /// -> [u8; 4] - RGBA pixel
+    /// -> [u8; 3] - RGB pixel
     pub fn get_pixel(&self, x: usize, y: usize) -> [u8; 3] {
         debug_assert!(
             x < self.width && y < self.height,
@@ -49,9 +49,9 @@ impl RgbPixmap {
         self.data[index..index + 3].try_into().unwrap()
     }
 
-    /// Fills the entire pixmap with the specified RGBA color.
+    /// Fills the entire pixmap with the specified RGB color.
     ///
-    /// * color : [u8; 4] - RGBA pixel
+    /// * color : [u8; 3] - RGB pixel
     pub fn fill(&mut self, color: [u8; 3]) {
         for chunk in self.data.chunks_mut(3) {
             chunk.copy_from_slice(&color);
