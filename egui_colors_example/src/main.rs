1228da2e35124c2d007b6247a8a23d0efc026f63
zombkit <stephen.korzen@gmail.com>
2025-09-25 00:12:56 -0700

    f

--- egui_colors_example/src/main.rs
+++ egui_colors_example/src/main.rs
@@ -43,7 +43,7 @@ struct MyApp {
 
 impl MyApp {
     fn new() -> Self {
-        let backend = SoftBackend::new_with_system_fonts(150, 100, 12);
+        let backend = SoftBackend::new_with_font(150, 100, "");
         let mut terminal = Terminal::new(backend).unwrap();
         let appik = App::default();
 
