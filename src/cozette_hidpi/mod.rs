51ddf3f8670bd18affb1e0b30f1522f4b16598f4
zombkit <stephen.korzen@gmail.com>
2025-09-26 16:41:53 -0700

    d

--- src/cozette_hidpi/mod.rs
+++
@@ -1,4 +0,0 @@
-mod mono_cozette_hidpi_all;
-mod mono_cozette_hidpi_all_atlas;
-pub use mono_cozette_hidpi_all::*;
-pub use mono_cozette_hidpi_all_atlas::*;
--- src/scientifica/mod.rs
+++
@@ -1,5 +0,0 @@
-mod mono_scientifica_all;
-
-mod mono_scientifica_all_atlas;
-pub use mono_scientifica_all::*;
-pub use mono_scientifica_all_atlas::*;
