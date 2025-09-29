ac6fd50fc6f6365cbc141629748e1b7d5e057e89
zombkit <stephen.korzen@gmail.com>
2025-09-29 01:41:53 -0700

    Reapply "soft backend good"
    This reverts commit 738a9d14332543b64df26fd607b48a4936f4150a.

--- src/embedded_backend.rs
+++ src/embedded_backend.rs
@@ -6,6 +6,7 @@ use std::io;
 
 use crate::colors::*;
 use crate::pixmap::RgbPixmap;
+use crate::soft_backend::RasterBackend;
 
 use embedded_graphics::Drawable;
 
@@ -29,38 +30,19 @@ pub struct EmbeddedGraphics {
     pub font_italic: Option<MonoFont<'static>>,
 }
 
-impl SoftBackend<EmbeddedGraphics> {
-    /// Retuns the raw rgb data of the pixmap as a flat array
-    pub fn get_pixmap_data(&self) -> &[u8] {
-        self.rgb_pixmap.data()
-    }
-    /// Retuns the pixmap in rgba format as a flat vector
-    pub fn get_pixmap_data_as_rgba(&self) -> Vec<u8> {
-        self.rgb_pixmap.to_rgba()
-    }
-    /// Returns the width of the pixmap in pixels
-    pub fn get_pixmap_width(&self) -> usize {
-        self.rgb_pixmap.width()
-    }
-    /// Returns the height of the pixmap in pixels
-    pub fn get_pixmap_height(&self) -> usize {
-        self.rgb_pixmap.height()
-    }
-
-    fn draw_cell(&mut self, xik: u16, yik: u16) {
-        let rat_cell = self.buffer.cell(Position::new(xik, yik)).unwrap();
-
+impl RasterBackend for EmbeddedGraphics {
+    fn draw_cell(&mut self, xik: u16, yik: u16, rat_cell: &Cell) {
         let mut rat_fg = rat_to_rgb(&rat_cell.fg, true);
         let mut rat_bg = rat_to_rgb(&rat_cell.bg, false);
 
         let mut style_builder = MonoTextStyleBuilder::new()
-            .font(&self.raster_backend.font_regular)
+            .font(&self.font_regular)
             .text_color(Rgb888::WHITE)
             .background_color(Rgb888::BLACK);
 
         for modifier in rat_cell.modifier.iter() {
             style_builder = match modifier {
-                style::Modifier::BOLD => match &self.raster_backend.font_bold {
+                style::Modifier::BOLD => match &self.font_bold {
                     None => style_builder,
                     Some(font) => style_builder.font(font),
                 },
@@ -117,7 +99,9 @@ impl SoftBackend<EmbeddedGraphics> {
         .draw(&mut self.rgb_pixmap)
         .unwrap();
     }
+}
 
+impl SoftBackend<EmbeddedGraphics> {
     /// Creates a new software backend with the given monospaced font(s).
     ///
     /// (new width height font-regular font-bold font-italic) -> SoftBackend
@@ -169,105 +153,5 @@ impl SoftBackend<EmbeddedGraphics> {
         };
         _ = return_struct.clear();
         return_struct
-    }
-
-    /// Returns a reference to the internal buffer of the `SoftBackend`.
-    pub const fn buffer(&self) -> &Buffer {
-        &self.buffer
-    }
-
-    /// Resizes the `SoftBackend` to the specified width and height.
-    pub fn resize(&mut self, width: u16, height: u16) {
-        self.buffer.resize(Rect::new(0, 0, width, height));
-        let rgb_pixmap = RgbPixmap::new(
-            self.char_width as usize * width as usize,
-            self.char_height as usize * height as usize,
-        );
-        self.rgb_pixmap = rgb_pixmap;
-        self.redraw();
-    }
-
-    /// Redraws the pixmap
-    pub fn redraw(&mut self) {
-        self.always_redraw_list = FxHashSet::default();
-        for x in 0..self.buffer.area.width {
-            for y in 0..self.buffer.area.height {
-                self.draw_cell(x, y);
-            }
-        }
-    }
-
-    fn update_blinking(&mut self) {
-        self.blink_counter = (self.blink_counter + 1) % 200;
-
-        self.blinking_fast = matches!(self.blink_counter % 100, 0..=5);
-        self.blinking_slow = matches!(self.blink_counter, 20..=25);
-    }
-}
-
-impl Backend for SoftBackend<EmbeddedGraphics> {
-    fn draw<'a, I>(&mut self, content: I) -> io::Result<()>
-    where
-        I: Iterator<Item = (u16, u16, &'a Cell)>,
-    {
-        self.update_blinking();
-        for (x, y, c) in content {
-            self.buffer[(x, y)] = c.clone();
-            self.draw_cell(x, y);
-        }
-        for (x, y) in self.always_redraw_list.clone().iter() {
-            self.draw_cell(*x, *y);
-        }
-
-        Ok(())
-    }
-
-    fn hide_cursor(&mut self) -> io::Result<()> {
-        self.cursor = false;
-
-        Ok(())
-    }
-
-    fn show_cursor(&mut self) -> io::Result<()> {
-        self.cursor = true;
-        Ok(())
-    }
-
-    fn get_cursor_position(&mut self) -> io::Result<Position> {
-        Ok(self.cursor_pos.into())
-    }
-
-    fn set_cursor_position<P: Into<Position>>(&mut self, position: P) -> io::Result<()> {
-        self.cursor_pos = position.into().into();
-        Ok(())
-    }
-
-    fn clear(&mut self) -> io::Result<()> {
-        self.buffer.reset();
-        let clear_cell = Cell::EMPTY;
-        let colorik = rat_to_rgb(&clear_cell.bg, false);
-
-        self.rgb_pixmap.fill([colorik[0], colorik[1], colorik[2]]);
-
-        Ok(())
-    }
-
-    fn size(&self) -> io::Result<Size> {
-        Ok(self.buffer.area.as_size())
-    }
-
-    fn window_size(&mut self) -> io::Result<WindowSize> {
-        let window_pixels = Size {
-            width: self.get_pixmap_width() as u16,
-            height: self.get_pixmap_height() as u16,
-        };
-        Ok(WindowSize {
-            columns_rows: self.buffer.area.as_size(),
-            pixels: window_pixels,
-        })
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        Ok(())
     }
 }
